aa70af66daa5c54e7c3a5579802eb15c5e0bd26d
Vicente Gonzalez <johndoe@example.com>
2020-11-04 16:25:51 -0300

    casi terminado

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minutesEl = document.getElementById('minutes')
+const secondsEl = document.getElementById('seconds')
 const newYears = '1 Jan 2021'
 
 function countDown(){
     const newYearsDate = new Date(newYears)
     const currentDate = new Date()
 
-    console.log(newYearsDate - currentDate)
-}
+    const totalSeconds =  (newYearsDate - currentDate) / 1000
+
+    const days = Math.floor(totalSeconds / 3600/ 24)
+    const hours = Math.floor(totalSeconds / 3600) % 24
+    const minutes = Math.floor(totalSeconds / 60) % 60
+    const seconds = Math.floor(totalSeconds) % 60
+
+    daysEl.innerHTML = days
+    hoursEl.innerHTML = hours
+    minutesEl.innerHTML = minutes
+    secondsEl.innerHTML = seconds
+}   
 
 countDown()
+
+setInterval(countDown, 1000)
